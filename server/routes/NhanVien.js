eaf83dabefc2ff926b5e0f0eff8c3f29d52dc592
tamsoul <tamsoul>
2024-08-14 21:22:04 +0700

    Test

--- server/routes/NhanVien.js
+++ server/routes/NhanVien.js
@@ -1,7 +1,6 @@
 const router = require('express').Router();
 const NhanVienController = require('../controllers/NhanVienController');
 
-// Route
 router.post('/register', NhanVienController.register);
 
 module.exports = router;
